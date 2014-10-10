4f147ab76a8a9ba6cbc3f4087c23e382827d080d
Andrew Cottam <a.cottam@gmail.com>
2014-10-10 15:31:23 +0200

    added loading icons

--- scripts/dario_lcc.js
+++ scripts/dario_lcc.js
@@ -5,7 +5,7 @@ require({
 		name : "jrc",
 		location : "/darios_lcc/scripts"
 	}]
-}, ["dojo/date/stamp", "dijit/registry", "dojo/ready", "dojo/parser", "dojo/_base/array", "esri/geometry/Extent", "esri/SpatialReference", "dojo/on", "jrc/GeeLayer", "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dijit/Calendar", "dijit/form/Button", "dijit/form/Select", "dijit/form/CheckBox"], function(stamp, registry, ready, parser, array, Extent, SpatialReference, on, GeeLayer) {
+}, ["dojo/dom-style", "dojo/date/stamp", "dijit/registry", "dojo/ready", "dojo/parser", "dojo/_base/array", "esri/geometry/Extent", "esri/SpatialReference", "dojo/on", "jrc/GeeLayer", "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dijit/Calendar", "dijit/form/Button", "dijit/form/Select", "dijit/form/CheckBox"], function(domStyle, stamp, registry, ready, parser, array, Extent, SpatialReference, on, GeeLayer) {
 	ready(function() {
 		var map, rgbLayer;
 		parser.parse().then(function() {
@@ -18,7 +18,7 @@ require({
 				lccLayer.setVisibility(!lccLayer.visible);
 			});
 		});
-		initExtent = new Extent(3801000,-783000,4095000,-644000, new SpatialReference({
+		initExtent = new Extent(3801000, -783000, 4095000, -644000, new SpatialReference({
 			wkid : 102100
 		}));
 		map = new esri.Map("map", {
@@ -32,14 +32,22 @@ require({
 				visible : true
 			});
 			rgbLayer.set("sceneid", "collection");
-			lccLayer = new GeeLayer('lccLayer', {
-				startDate : stamp.toISOString(registry.byId("startcal").value).substr(0, 10),
-				endDate : stamp.toISOString(registry.byId("endcal").value).substr(0, 10),
-				collectionid : registry.byId("collectionSelect").value,
-				visible : true
-			});
+			lccLayer = new GeeLayer('lccLayer');
+			setLayerParams(lccLayer);
 			lccLayer.set("sceneid", "dario");
 			map.addLayers([rgbLayer, lccLayer]);
+			on(rgbLayer, "update-start", function() {
+				domStyle.set("loadingrgb", "display", "inline-block");
+			});
+			on(rgbLayer, "update-end", function() {
+				domStyle.set("loadingrgb", "display", "none");
+			});
+			on(lccLayer, "update-start", function() {
+				domStyle.set("loadingdetection", "display", "inline-block");
+			});
+			on(lccLayer, "update-end", function() {
+				domStyle.set("loadingdetection", "display", "none");
+			});
 		});
 		function extentChange(event) {
 			console.log("extentChange (x=" + map.extent.getCenter().x + " y=" + map.extent.getCenter().y + ")");
@@ -51,10 +59,17 @@ require({
 			});
 		}
 
+		function setLayerParams(layer) {
+			layer.set("startDate", stamp.toISOString(registry.byId("startcal").value).substr(0, 10));
+			layer.set("endDate", stamp.toISOString(registry.byId("endcal").value).substr(0, 10));
+			layer.set("collectionid", registry.byId("collectionSelect").value);
+		};
+
 		function getMap() {
 			array.forEach(map.layerIds, function(item) {
 				var layer = map.getLayer(item);
 				if (layer.declaredClass === 'jrc/GeeLayer') {
+					setLayerParams(layer);
 					layer.refresh();
 				};
 			});
